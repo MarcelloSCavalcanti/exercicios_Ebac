536b37a215e604b57f651e498f85d1e8f608ec81
Marcello_ubuntu <marcellonovaes98@gmail.com>
2024-05-24 19:23:12 -0300

    jquery task

--- js/main.js
+++ js/main.js
@@ -1,12 +1,6 @@
 $(document).ready(function() {
-    $("#cpf").mask("000.000.000-00", {
-        placeholder: "111.222.333-44"
-    })
     $("#tel").mask("(00) 0 0000-0000", {
         placeholder: "(21) 9 8765-4321"
-    })
-    $("#cep").mask("00000-000", {
-        placeholder: "12345-678"
     })
     $("#cadastro").validate({
         rules: {
@@ -19,21 +13,13 @@ $(document).ready(function() {
                 email:true,
             },
             tel: {
-                required: true,
+                required: false,
                 minlength: 16,
             },
-            address: {
+            message: {
                 required: true,
                 minlength: 10,
             },
-            cep: {
-                required: true,
-                minlength: 8,
-            },
-            cpf: {
-                required: true,
-                minlength: 14,
-            }
         },
         messages: {
             cName: "Por favor insira seu nome completo!",
